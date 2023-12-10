0e92158d3fd5d52e7c92af580fe13372d25a75c3
medinmujkovic <medin.mujkovic@hotmail.com>
2023-12-10 22:27:54 +0100

    First version of the login UI

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,9 +1,29 @@
 package ba.unsa.etf.rpr;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
-public class App {
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+
+public class App extends Application {
     public static void main(String[] args) {
-
+            launch(args);
         }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        stage.setTitle("Login");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+
+        //stage.getIcons().add(new Image(""));
+        stage.setResizable(false);
+        stage.show();
     }
+}
 
---
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -0,0 +1,19 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class LoginController {
+    public Button loginId;
+    public TextField usernameId;
+
+    public void loginAction(ActionEvent actionEvent) {
+        //listener
+        //initialize
+    }
+}
