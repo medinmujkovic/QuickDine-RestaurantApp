7787139a26a68a3a810b20f4fceae9770803083c
medinmujkovic <medin.mujkovic@hotmail.com>
2023-12-13 00:23:28 +0100

    Added utilis and cleaned the code

---
+++ src/main/java/ba/unsa/etf/rpr/utils/Stage.java
@@ -0,0 +1,19 @@
+package ba.unsa.etf.rpr.utils;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class Stage {
+
+    public void start(String resource, String title) throws Exception {
+        javafx.stage.Stage stage=new javafx.stage.Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(resource));
+        stage.setTitle(title);
+        stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(true);
+        stage.show();
+    }
+}
