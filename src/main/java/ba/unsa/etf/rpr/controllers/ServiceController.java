230da8171f7b3c21aab5ae76a1f4cdff9a7c6629
medinmujkovic <medin.mujkovic@hotmail.com>
2023-12-22 10:27:14 +0100

    Added status light indication, fixed UI

--- src/main/java/ba/unsa/etf/rpr/controllers/ServiceController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ServiceController.java
@@ -7,6 +7,9 @@ import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 import static ba.unsa.etf.rpr.utils.MenuRequestHelper.createMenuRequests;
 
@@ -39,18 +42,46 @@ public class ServiceController {
                 }
                 else{
                     HBox hbox=new HBox(10);
+
                     ImageView imageView=new ImageView(item.path());
                     imageView.setFitWidth(80);
                     imageView.setFitHeight(80);
+
+                    Circle statusCircle = new Circle(8);
+                    statusCircle.setFill(item.Status().equals("Available") ? Color.GREEN : Color.RED);
+                    statusCircle.setStroke(Color.BLACK);
+                    statusCircle.setStrokeWidth(0.8);
+
                     HBox hBox=new HBox();
+                    HBox info1=new HBox(
+                            new Label(item.name())
+                    );
+                    HBox info2=new HBox(
+                            new Label("$" + item.price())
+                    );
+                    VBox info3=new VBox(
+                            new Button("Add"),
+                            new Spinner(0, 30, 0)
+                    );
                     hBox.setSpacing(70);
                     hBox.getChildren().addAll(
-                            new Label(item.name()),
-                            new Label("$"+item.price()),
-                            new Label(item.Status()),
-                            new Button("Add"),
-                            new Spinner(0, 10, 0)
+                            info1,
+                            info2,
+                            info3,
+                            statusCircle
                     );
+
+                    info1.setMinWidth(50);
+                    info1.setPrefWidth(50);
+                    info1.setMaxWidth(50);
+                    info2.setMinWidth(60);
+                    info2.setPrefWidth(60);
+                    info2.setMaxWidth(60);
+                    info3.setMinWidth(60);
+                    info3.setPrefWidth(60);
+                    info3.setMaxWidth(60);
+                    info3.setSpacing(5);
+
                     hbox.getChildren().addAll(imageView,hBox);
                     setGraphic(hbox);
                 }
