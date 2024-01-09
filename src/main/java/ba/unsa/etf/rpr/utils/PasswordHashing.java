87d3c86d5acdc5a7953b6ac239e424f61556ba61
MersidPi <mpilipovic1@etf.unsa.ba>
2024-01-09 23:07:39 +0100

    bcrypt refactoring

--- src/main/java/ba/unsa/etf/rpr/utils/PasswordHashing.java
+++ src/main/java/ba/unsa/etf/rpr/utils/PasswordHashing.java
@@ -1,37 +1,11 @@
 package ba.unsa.etf.rpr.utils;
 
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import org.mindrot.jbcrypt.BCrypt;
 
 public class PasswordHashing {
 
-    public static String hashString(String input) throws NoSuchAlgorithmException {
+    public static String hashString(String input) {
         return BCrypt.hashpw(input, BCrypt.gensalt());
-
-
-
-
-
-
-
-
-        /*
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        byte[] hashBytes = digest.digest(input.getBytes(StandardCharsets.UTF_8));
-
-        // Convert bytes to hexadecimal representation
-        StringBuilder hexString = new StringBuilder();
-        for (byte hashByte : hashBytes) {
-            String hex = Integer.toHexString(0xff & hashByte);
-            if (hex.length() == 1) {
-                hexString.append('0');
-            }
-            hexString.append(hex);
-        }
-
-        return hexString.toString();*/
     }
 
     public static boolean isPasswordCorrect(String enteredPassword, String storedHash) {
